8b8983dd8a25d78def2f299a006b6c47774aaec7
Jeison Solarte <jeisonsolarte@gmail.com>
2019-04-12 20:59:44 -0500

    Avance Trabajo Uno

--- app/src/main/java/co/jeisonsolarte/apirestproyect/MainActivity.java
+++ app/src/main/java/co/jeisonsolarte/apirestproyect/MainActivity.java
@@ -5,28 +5,60 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    Button button;
-
-    String nombre;
+    ImageView imageView;
+    TextView edtCambiarFoto;
+    Boolean ban;
+    Button button1;
+    RadioGroup radioGroup;
+    EditText edtNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText=findViewById(R.id.edtnombre);
-        button=findViewById(R.id.btn);
+        imageView=findViewById(R.id.main_img);
+        edtCambiarFoto=findViewById(R.id.main_subirFoto);
+        ban=true;
+        radioGroup=findViewById(R.id.main_radiog);
+        button1=findViewById(R.id.btn);
+        edtNombre=findViewById(R.id.edtnombre);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        edtCambiarFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nombre=editText.getText().toString();
-                Toast.makeText(MainActivity.this, "El nombre es "+nombre, Toast.LENGTH_SHORT).show();
+                if (ban){
+                    imageView.setImageResource(R.drawable.ic_launcher_foreground);
+                    ban=false;
+                }
+                else{
+                    imageView.setImageResource(R.mipmap.ic_launcher);
+                    ban=true;
+                }
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (R.id.radio1 == checkedId)
+                    Toast.makeText(MainActivity.this, "Checked Radio 1", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Checked Radio 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "ok "+edtNombre.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
